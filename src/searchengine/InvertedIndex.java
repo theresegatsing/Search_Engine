8a88e4eeac154e23f500874d364fec335baddb5e
Therese Elvira Mombou Gatsing <tmombougatsing@mail.wlu.edu>
2025-11-22 20:07:55 -0500

    Initialised InvertedIndex class

--- src/searchengine/InvertedIndex.java
+++ src/searchengine/InvertedIndex.java
@@ -1,5 +1,15 @@
 package searchengine;
 
+import java.util.*;
+
+/**
+ * Inverted index maps terms -> documents that contain those terms.
+ *
+ * For example:
+ *   "java" -> {doc1, doc3}
+ *   "search" -> {doc2}
+ */
+
 public class InvertedIndex {
 
 }
